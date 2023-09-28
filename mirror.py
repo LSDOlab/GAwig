239fdce3c8ed5bad19edc0fe0f46521474d29f88
Nicholas Orndorff <norndorff@live.com>
2023-09-28 14:05:31 -0700

    edits

--- mirror.py
+++ mirror.py
@@ -16,6 +16,7 @@ class Mirror(m3l.ExplicitOperation):
         self.parameters.declare('mesh_name')
         self.parameters.declare('ns')
         self.parameters.declare('nc')
+        self.parameters.declare('point')
 
     def assign_attributes(self):
         self.component = self.parameters['component']
@@ -23,44 +24,87 @@ class Mirror(m3l.ExplicitOperation):
         self.mesh_name = self.parameters['mesh_name']
         self.ns = self.parameters['ns']
         self.nc = self.parameters['nc']
+        self.point = self.parameters['point']
 
     def compute(self):
         mesh_name = self.parameters['mesh_name']
         ns = self.parameters['ns']
         nc = self.parameters['nc']
-        csdl_model = MirrorCSDL(module=self,mesh_name=mesh_name,ns=ns,nc=nc)
+        point = self.parameters['point']
+        csdl_model = MirrorCSDL(module=self,mesh_name=mesh_name,ns=ns,nc=nc,point=point)
         return csdl_model
-    
+
     def evaluate(self):
         mesh_name = self.parameters['mesh_name']
         ns = self.parameters['ns']
         nc = self.parameters['nc']
-
+        point = self.parameters['point']
+ 
         self.name = 'mirror'
         self.arguments = {}
-        
+
         mirror_mesh = m3l.Variable(mesh_name + '_mirror', shape=(ns,nc,3), operation=self)
 
         return mirror_mesh
 
+ 
+
+ 
 
 class MirrorCSDL(ModuleCSDL):
     def initialize(self):
         self.parameters.declare('mesh_name')
         self.parameters.declare('ns')
         self.parameters.declare('nc')
-
+        self.parameters.declare('point')
+ 
     def define(self):
         mesh_name = self.parameters['mesh_name']
         ns = self.parameters['ns']
         nc = self.parameters['nc']
+        point = self.parameters['point']
+
+        alpha = self.register_module_input('alpha', shape=(1), promotes=True, val=0)
+
+        rotation_matrix_y = self.create_output('rotation_matrix_y',shape=(3,3),val=0)
+        rotation_matrix_y[0,0] = csdl.reshape(csdl.cos(alpha), (1,1))
+        rotation_matrix_y[0,2] = csdl.reshape(csdl.sin(alpha), (1,1))
+        rotation_matrix_y[1,1] = self.create_input('one',shape=(1,1),val=1)
+        rotation_matrix_y[2,0] = csdl.reshape(-csdl.sin(alpha), (1,1))
+        rotation_matrix_y[2,2] = csdl.reshape(csdl.cos(alpha), (1,1))
+
 
         #mesh = self.declare_variable('mesh', shape=(ns,nc,3))
         mesh = self.register_module_input(mesh_name, shape=(ns,nc,3), promotes=True)
 
-        mirror_mesh = self.create_output(mesh_name + '_mirror', shape=(ns,nc,3), val=0)
-        mirror_mesh[:,:,0] = mesh[:,:,0]
-        mirror_mesh[:,:,1] = mesh[:,:,1]
-        mirror_mesh[:,:,2] = -1*mesh[:,:,2]
+        translated_mesh_points = mesh - np.tile(point, (ns, nc, 1))
+        #expanded_rotation_matrix_y = csdl.expand(rotation_matrix_y, (ns,nc,3,3), 'ij->abij')
+        # rotated_mesh_points = translated_mesh_points.dot(rotation_matrix_z.dot(rotation_matrix_y.dot(rotation_matrix_x)))
+        #rotated_mesh_points = csdl.matvec(expanded_rotation_matrix_y, translated_mesh_points)
+
+
+        rotated_mesh_points = self.create_output('rotated_mesh_points', shape=(ns,nc,3), val=0)
+        for i in range(ns):
+            for j in range(nc):
+                eval_point = csdl.reshape(translated_mesh_points[i,j,:], (3))
+                rotated_mesh_points[i,j,:] = csdl.reshape(csdl.matvec(rotation_matrix_y, eval_point), (1,1,3))
+
+        self.print_var(rotated_mesh_points)
+        #self.print_var(mesh)
+
+        rotated_mesh = rotated_mesh_points + np.tile(point, (ns, nc, 1))
+        self.print_var(translated_mesh_points[-1,-1,:])
+        self.print_var(rotation_matrix_y)
+
+
+        #mirror_mesh = self.create_output(mesh_name + '_mirror', shape=(ns,nc,3), val=0)
+
+        #mirror_mesh[:,:,0] = mesh[:,:,0]
+
+        #mirror_mesh[:,:,1] = mesh[:,:,1]
+
+        #mirror_mesh[:,:,2] = -1*mesh[:,:,2]
+
+ 
 
         # self.print_var(mirror_mesh)
--- run_clarky.py
+++ run_clarky.py
@@ -6,7 +6,6 @@ from modopt.csdl_library import CSDLProblem
 from caddee.core.caddee_core.system_representation.component.component import LiftingSurface, Component
 import array_mapper as am
 import lsdo_geo as lg
-from lsdo_rotor.core.BEM_caddee.BEM_caddee import BEM, BEMMesh
 from VAST.core.vast_solver import VASTFluidSover
 from VAST.core.fluid_problem import FluidProblem
 from VAST.core.generate_mappings_m3l import VASTNodalForces
@@ -16,23 +15,17 @@ plt.rcParams.update(plt.rcParamsDefault)
 import csdl
 from mirror import Mirror
 
-
-
+ 
 
 file_name = 'GAwig/clarky.stp'
-
 caddee = cd.CADDEE()
 caddee.system_model = system_model = cd.SystemModel()
 caddee.system_representation = sys_rep = cd.SystemRepresentation()
 caddee.system_parameterization = sys_param = cd.SystemParameterization(system_representation=sys_rep)
-
 spatial_rep = sys_rep.spatial_representation
 spatial_rep.import_file(file_name=file_name)
 spatial_rep.refit_geometry(file_name=file_name)
 # spatial_rep.plot(plot_types=['mesh'])
-
-
-
 
 # wing
 wing_primitive_names = list(spatial_rep.get_primitives(search_names=['WingGeom']).keys())
@@ -40,8 +33,7 @@ wing = LiftingSurface(name='wing', spatial_representation=spatial_rep, primitive
 sys_rep.add_component(wing)
 # wing.plot()
 
-
-
+ 
 
 # wing mesh
 num_spanwise_vlm = 18
@@ -52,12 +44,60 @@ chord_surface = am.linspace(leading_edge, trailing_edge, num_chordwise_vlm)
 #spatial_rep.plot_meshes([chord_surface])
 
 
+ 
+
 wing_upper_surface_wireframe = wing.project(chord_surface.value + np.array([0., 0., 1.]), direction=np.array([0., 0., -1.]), grid_search_n=30, plot=False)
 wing_lower_surface_wireframe = wing.project(chord_surface.value - np.array([0., 0., 1.]), direction=np.array([0., 0., 1.]), grid_search_n=30, plot=False)
 wing_camber_surface = am.linspace(wing_upper_surface_wireframe, wing_lower_surface_wireframe, 1)
-spatial_rep.plot_meshes([wing_camber_surface])
+#spatial_rep.plot_meshes([wing_camber_surface])
 wing_vlm_mesh_name = 'wing_vlm_mesh'
 sys_rep.add_output(wing_vlm_mesh_name, wing_camber_surface)
 
 
 sys_param.setup()
+
+ 
+
+ 
+
+# design scenario
+design_scenario = cd.DesignScenario(name='wig')
+wig_model = m3l.Model()
+wig_condition = cd.CruiseCondition(name='wig')
+wig_condition.atmosphere_model = cd.SimpleAtmosphereModel()
+wig_condition.set_module_input(name='altitude', val=0)
+wig_condition.set_module_input(name='mach_number', val=0.15, dv_flag=True, lower=0.1, upper=0.3)
+wig_condition.set_module_input(name='range', val=1000)
+wig_condition.set_module_input(name='pitch_angle', val=np.deg2rad(0), dv_flag=True, lower=np.deg2rad(-10), upper=np.deg2rad(10))
+wig_condition.set_module_input(name='flight_path_angle', val=0)
+wig_condition.set_module_input(name='roll_angle', val=0)
+wig_condition.set_module_input(name='yaw_angle', val=0)
+wig_condition.set_module_input(name='wind_angle', val=0)
+wig_condition.set_module_input(name='observer_location', val=np.array([0, 0, 1000]))
+ac_states = wig_condition.evaluate_ac_states()
+wig_model.register_output(ac_states)
+ 
+
+ 
+
+# create a mirrored mesh
+mirror = Mirror(component=wing,mesh_name=wing_vlm_mesh_name,ns=num_spanwise_vlm,nc=num_chordwise_vlm,point=np.array([0,0,0]))
+wing_camber_surface_mirror = mirror.evaluate()
+wig_model.register_output(wing_camber_surface_mirror)
+
+
+ 
+
+# add the cruise m3l model to the cruise condition
+wig_condition.add_m3l_model('wig_model', wig_model)
+# add the design condition to the design scenario
+design_scenario.add_design_condition(wig_condition)
+system_model.add_design_scenario(design_scenario=design_scenario)
+caddee_csdl_model = caddee.assemble_csdl()
+
+ 
+
+ 
+
+sim = Simulator(caddee_csdl_model, analytics=True)
+sim.run()
