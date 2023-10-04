1ba23ea4e5bcc00bfb74dbc3ea8ea31e60f6331a
Nicholas Orndorff <norndorff@live.com>
2023-10-03 17:47:24 -0700

    new valication model

---
+++ run_naca.py
@@ -0,0 +1,32 @@
+import caddee.api as cd
+import m3l
+from python_csdl_backend import Simulator
+from modopt.scipy_library import SLSQP
+from modopt.csdl_library import CSDLProblem
+from caddee.core.caddee_core.system_representation.component.component import LiftingSurface, Component
+import array_mapper as am
+import lsdo_geo as lg
+from VAST.core.vast_solver import VASTFluidSover
+from VAST.core.fluid_problem import FluidProblem
+from VAST.core.generate_mappings_m3l import VASTNodalForces
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update(plt.rcParamsDefault)
+import csdl
+from mirror import Mirror
+
+from mpl_toolkits.mplot3d import proj3d
+
+
+
+ 
+
+file_name = 'GAwig/naca6409.stp'
+caddee = cd.CADDEE()
+caddee.system_model = system_model = cd.SystemModel()
+caddee.system_representation = sys_rep = cd.SystemRepresentation()
+caddee.system_parameterization = sys_param = cd.SystemParameterization(system_representation=sys_rep)
+spatial_rep = sys_rep.spatial_representation
+spatial_rep.import_file(file_name=file_name)
+spatial_rep.refit_geometry(file_name=file_name)
+# spatial_rep.plot(plot_types=['mesh'])
