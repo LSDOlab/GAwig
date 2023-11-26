d47558b80e355befd4db68675be04849dc68735f
Nicholas Orndorff <norndorff@live.com>
2023-11-25 22:54:18 -0800

    clean up

--- mirror.py
+++ mirror.py
@@ -103,7 +103,6 @@ class MirrorCSDL(ModuleCSDL):
         dh = self.create_output('dh', shape=(nt,nc,ns,3), val=0)
         dh[:,:,:,2] = csdl.expand(h, (nt,nc,ns,1), 'i->abci')
 
-        #mesh = rotated_mesh + dh
         #mesh = rotated_mesh_points + dh
         mesh = self.register_output(mesh_name+'_out', rotated_mesh + dh)
 
